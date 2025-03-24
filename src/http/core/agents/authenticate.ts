aeec832ca1e12b0c60001adb2e1a4960d4b19cd8
OAB Seccional do Maranhão <informatica@oabma.org.br>
2025-03-24 17:39:07 -0300

    criando vercel.json

--- src/http/core/agents/authenticate.ts
+++ src/http/core/agents/authenticate.ts
@@ -75,6 +75,7 @@ export async function authenticate(app: FastifyInstance) {
         .setCookie('@lexhub-auth', token, {
           path: '/',
           httpOnly: true,
+          sameSite: 'none',
           maxAge: 60 * 60 * 24, // 1 day
         })
         .status(201)
