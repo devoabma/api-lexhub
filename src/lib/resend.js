2a7bca8332e4cdb0a6f8d93e0d84b131ea2d3f68
OAB Seccional do Maranhão <informatica@oabma.org.br>
2025-03-28 14:59:29 -0300

    removido .js dos codigos

--- src/lib/resend.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.resend = void 0;
-const _env_1 = require("http/_env");
-const resend_1 = require("resend");
-exports.resend = new resend_1.Resend(_env_1.env.RESEND_API_KEY);
