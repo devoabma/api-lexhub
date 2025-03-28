104821ff0b6b233c321029cf1344b425ea0106f9
OAB Seccional do Maranhão <informatica@oabma.org.br>
2025-03-28 14:42:05 -0300

    trocado variveis de ambiente em prismaa

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -5,5 +5,5 @@ const client_1 = require("@prisma/client");
 const _env_1 = require("http/_env");
 exports.prisma = new client_1.PrismaClient({
     // Gera logs de queries apenas em ambiente de desenvolvimento
-    log: _env_1.env.NODE_ENV === 'DEVELOPMENT' ? ['query'] : [],
+    log: _env_1.env.NODE_ENV === 'development' ? ['query'] : [],
 });
