d1592a20c1f61961586e70095b9fa51d5a49ea0b
OAB Seccional do Maranhão <informatica@oabma.org.br>
2026-09-11 22:23:36 -0300

    chore: ajuste pm2 lexhub-api 2

--- src/http/server.ts
+++ src/http/server.ts
@@ -9,6 +9,6 @@ app
   .then(() => {
     console.log(`
     🚀 \x1b[32m> Servidor iniciado com sucesso!\x1b[0m ✨
-    📡 \x1b[33m> Aguardando conexões na porta ${env.PORT}...\x1b[0m
+    📡 \x1b[33m> Aguardando conexões na porta ${env.PORT}\x1b[0m
     `)
   })
